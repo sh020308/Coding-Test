8e2d54e13ffce418b8197709947956f8226c968a
pakafans <75054298+sh020308@users.noreply.github.com>
2020-12-09 15:22:13 +0900

    Level1
    long타입으로 변환 후 곱해준다.

---
+++ 프로그래머스 레벨1/x만큼 간격이 있는 n개의 숫자.java
@@ -0,0 +1,12 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12954
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long temp = x;
+        for(int i=0;i<n;i++)
+        {
+            answer[i] = temp*(i+1);
+        }
+        return answer;
+        }
+}
