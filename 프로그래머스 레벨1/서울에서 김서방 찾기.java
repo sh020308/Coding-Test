c33893d97da4e734f959847fa73b7066bd67cd9d
pakafans <75054298+sh020308@users.noreply.github.com>
2020-12-09 14:18:21 +0900

    Level1
    Kim이라는 단어가 어디 배열에 있는지 찾는 문제

---
+++ 프로그래머스 레벨1/서울에서 김서방 찾기.java
@@ -0,0 +1,13 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12919
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        for(int i=0;i<seoul.length;i++) {
+            if("Kim".equals(seoul[i]))
+            {
+                answer += "김서방은 " + i + "에 있다";
+            }
+        }
+        return answer;
+    }
+}
