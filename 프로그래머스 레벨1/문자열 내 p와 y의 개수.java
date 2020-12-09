d9a762fccec784ec1443e32d259cc11f59edca34
pakafans <75054298+sh020308@users.noreply.github.com>
2020-12-09 14:12:45 +0900

    Level1
    조건식 or을 이용해 p와 y가 몇개있는지 카운트해서 맞으면 true 아니면 false

---
+++ 프로그래머스 레벨1/문자열 내 p와 y의 개수.java
@@ -0,0 +1,22 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12916
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+        int p = 0;
+        int y = 0;
+        
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+                p++;
+            }
+            else if(s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
+                y++;
+            }
+        }
+        if(p == y) {
+            answer = true;
+        }
+
+        return answer;
+    }
+}
