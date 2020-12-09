d0eac0154077a3efa145da55c86d2d518872ce2f
pakafans <75054298+sh020308@users.noreply.github.com>
2020-12-09 14:44:57 +0900

    Level1
    문자열을 정수로바꾸는 Integer.parseInt static 메소드를 쓴다.

---
+++ 프로그래머스 레벨1/문자열을 정수로 바꾸기.java
@@ -0,0 +1,7 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+class Solution {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
