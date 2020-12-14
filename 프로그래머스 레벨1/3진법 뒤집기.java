d3844e8d098294fc2b8f01eb9ee610bf22a9ede7
pakafans <75054298+sh020308@users.noreply.github.com>
2020-12-14 23:22:36 +0900

    Level1
    ﻿2시간 10분동안 푼문제..

--- 프로그래머스 레벨1/3진법 뒤집기.java
+++ 프로그래머스 레벨1/3진법 뒤집기.java
@@ -1,26 +1,25 @@
 // https://programmers.co.kr/learn/courses/30/lessons/68935
 class Solution {
     public int solution(int n) {
-		int count = 0;
-		int result = 0;
-		int a=1;
-		String su = "";
-		String temp2 = "";
+		int count = 0; // 자리수 계산
+		int result = 0; // 반환해줄 결과값
+		int a=1; // 다시 10진법으로 계산할때 쓸 변수
+		String su = ""; // 3진법으로 계산할때 쓸 string형
 		
-		for(int i=1;i<=n;i*=3) {
+		for(int i=1;i<=n;i*=3) { // 3진법 자리수 계산
 			count++;
 		}
 		
-		int[][] temp = new int[count][2];
-		count = 0;
+		int[][] temp = new int[count][2]; // 2차원 *2까지 저장해주는 2차원 배열 생성
+		count = 0; // 0으로 대입
 		
-		for(int i=1;i<=n;i*=3) {
+		for(int i=1;i<=n;i*=3) { // 2차원 배열에 0에는 i값 1에는 i*2값을 넣어준다.
 			temp[count][0] = i;
 			temp[count][1] = i*2;
 			count++;
 		}
 		
-		for(int i=temp.length-1;i>=0;i--) {
+		for(int i=temp.length-1;i>=0;i--) { // temp[i][1]보다 크면 2를 ...[0]보다 크면 1을 아니면 0을 더해준다.
 			if(temp[i][1] <= n) {
 				su += "2";
 				n -= temp[i][1];
@@ -36,7 +35,7 @@ class Solution {
 
 		
 		for(int i=1;i<=su.length();i++) {
-			result += Integer.parseInt(String.valueOf(su.charAt(i-1)))*a;
+			result += Integer.parseInt(String.valueOf(su.charAt(i-1)))*a; // char형을 int형으로 변환후 더해준다.
 			a*=3;
 		}
 		
