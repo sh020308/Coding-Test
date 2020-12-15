d048a65c4a11b391b714d88f3461fba9e07e6c67
pakafans <75054298+sh020308@users.noreply.github.com>
2020-12-16 00:43:45 +0900

    Level1
    d배열을 오름차순해주고 budget-d[i]가 0보다 작아질때까지 계속 빼주고 카운트해준다.

---
+++ 프로그래머스 레벨1/예산.java
@@ -0,0 +1,16 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12982
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for(int i=0;i<d.length;i++) {
+            if(budget-d[i] < 0) {
+                break;
+            }
+            budget -= d[i];
+            answer++;
+        }
+        return answer;
+    }
+}
